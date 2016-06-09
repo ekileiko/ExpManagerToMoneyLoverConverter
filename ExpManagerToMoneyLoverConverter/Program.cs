a7907bada98caaf73e9e586c77a88f7cff9c9e53
Eugene Kileiko <e.kileiko@gmail.com>
2016-06-09 23:31:46 +0300

    Started converting implemention

--- ExpManagerToMoneyLoverConverter/Program.cs
+++ ExpManagerToMoneyLoverConverter/Program.cs
@@ -1,15 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ExpManagerToMoneyLoverConverter.Helpers;
+using ExpManagerToMoneyLoverConverter.Models;
 
 namespace ExpManagerToMoneyLoverConverter
 {
     class Program
     {
+        private static readonly string MoneyLoverXml = File.ReadAllText("./Content/MoneyLoverExample.xml");
+        private static readonly string ExpManagerXml = File.ReadAllText("./Content/ExpManagerExample.xml");
+
+        private static readonly MoneyLoverModel MoneyLoverModel = SimpleSerializer.DeserializeXmlToObject<MoneyLoverModel>(MoneyLoverXml);
+        private static readonly ExpManagerModel ExpManagerModel = SimpleSerializer.DeserializeXmlToObject<ExpManagerModel>(ExpManagerXml);
+        
         static void Main(string[] args)
         {
+            StartConverting();
+        }
+
+        private static void StartConverting()
+        {
+            ConvertCategories();
+        }
+
+        static void ConvertCategories()
+        {
+            MoneyLoverModel.Tables.FirstOrDefault(t => t.Name == "categories")?.Rows.Clear();
         }
     }
 }
